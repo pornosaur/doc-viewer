bc273856f5d95bd10a9bf2184c9cacb1650cb569
Patrik Patera <pat.patera@gmail.com>
2019-07-19 14:12:40 +0800

    #3 - Resizing rectangles

--- ui-doc-viewer/ui-widgets/include/template_rect.h
+++ ui-doc-viewer/ui-widgets/include/template_rect.h
@@ -18,12 +18,16 @@ namespace qview {
     class TemplateRect : public QGraphicsRectItem {
 
     private:
-        bool contains = false;
+        bool contains = false, resizing = false;
 
         static QPen selected_pen, unselected_pen;
 
+        QGraphicsRectItem *resize_button;
+
 
         void selecting_area(bool select = true);
+
+        void resize_area(const QPointF &pos);
 
         void init();
 
@@ -31,6 +35,14 @@ namespace qview {
         void hoverMoveEvent(QGraphicsSceneHoverEvent *event) override;
 
         void hoverLeaveEvent(QGraphicsSceneHoverEvent *event) override;
+
+        void mousePressEvent(QGraphicsSceneMouseEvent *event) override;
+
+        void mouseMoveEvent(QGraphicsSceneMouseEvent *event) override;
+
+        void mouseReleaseEvent(QGraphicsSceneMouseEvent *event) override;
+
+        QVariant itemChange(GraphicsItemChange change, const QVariant &value);
 
     public:
         explicit TemplateRect(const QRectF &rect, QGraphicsItem *parent = nullptr);
--- ui-doc-viewer/ui-widgets/src/template_rect.cpp
+++ ui-doc-viewer/ui-widgets/src/template_rect.cpp
@@ -29,47 +29,97 @@ void TemplateRect::init() {
 
     setFlag(QGraphicsItem::ItemIsSelectable, true);
     setFlag(QGraphicsItem::ItemIsMovable, true);
+    setFlag(QGraphicsItem::ItemSendsGeometryChanges, true);
+
 
     auto lb_p = rect().bottomRight();
-    auto *it = new QGraphicsRectItem(lb_p.x() - 5, lb_p.y() - 5, 10, 10, this);
-    childItems().push_back(it);
+    resize_button = new QGraphicsRectItem(lb_p.x() - 10, lb_p.y() - 10, 10, 10, this);
+    childItems().push_back(resize_button);
+    resize_button->setVisible(false);
 
-    QPen p(QBrush(QColor(0, 0, 0, 255)), 2);
-    setPen(p);
-
-    QBrush b(QColor(0, 0, 0, 20), Qt::Dense3Pattern);
-    setBrush(b);
+    setPen(unselected_pen);
+    setBrush(QBrush(QColor(0, 0, 0, 20), Qt::Dense3Pattern));
 }
 
 void TemplateRect::selecting_area(bool select) {
     if (!contains && select) {
         contains = true;
 
-        setPen(selected_pen);
-        setCursor(QCursor(Qt::CursorShape::OpenHandCursor));
+        //setPen(selected_pen);
+        setCursor(QCursor(Qt::SizeFDiagCursor));
     } else if (contains && !select) {
         contains = false;
 
-        setPen(unselected_pen);
-        setCursor(QCursor(Qt::CursorShape::ArrowCursor));
+        //setPen(unselected_pen);
+        setCursor(QCursor(Qt::ArrowCursor));
     }
 }
 
 
+void TemplateRect::resize_area(const QPointF &pos) {
+    prepareGeometryChange();
+
+    QRectF new_rect(rect());
+
+    qreal w = (pos.x() - rect().x());
+    w = w <= 0 ? rect().width() : w;
+
+    qreal h = (pos.y() - rect().y());
+    h = h <= 0 ? rect().height() : h;
+
+    new_rect.setWidth(w);
+    new_rect.setHeight(h);
+    new_rect = new_rect.normalized();
+
+    resize_button->setRect(new_rect.bottomRight().x() - resize_button->rect().width(),
+                           new_rect.bottomRight().y() - resize_button->rect().height(),
+                           resize_button->rect().width(), resize_button->rect().height());
+
+    setRect(new_rect);
+}
+
 void TemplateRect::hoverMoveEvent(QGraphicsSceneHoverEvent *event) {
     QGraphicsRectItem::hoverMoveEvent(event);
 
-    QRectF m_rect(event->pos().x() - 20, event->pos().y() - 20, 40, 40);
-
-    if (!rect().contains(m_rect)) {
-        selecting_area();
-    } else {
-        selecting_area(false);
-    }
+    selecting_area(isSelected() && resize_button->contains(event->pos()));
 }
 
 void TemplateRect::hoverLeaveEvent(QGraphicsSceneHoverEvent *event) {
     QGraphicsRectItem::hoverLeaveEvent(event);
 
     selecting_area(false);
+}
+
+
+void TemplateRect::mousePressEvent(QGraphicsSceneMouseEvent *event) {
+    QGraphicsRectItem::mousePressEvent(event);
+
+    if (!resizing && resize_button->contains(event->pos())) {
+        resizing = true;
+        setFlag(QGraphicsItem::ItemIsMovable, false);
+    }
+}
+
+void TemplateRect::mouseMoveEvent(QGraphicsSceneMouseEvent *event) {
+    QGraphicsItem::mouseMoveEvent(event);
+
+    if (resizing) resize_area(event->pos());
+}
+
+void TemplateRect::mouseReleaseEvent(QGraphicsSceneMouseEvent *event) {
+    QGraphicsItem::mouseReleaseEvent(event);
+
+    if (resizing) {
+        setFlag(QGraphicsItem::ItemIsMovable, true);
+        resizing = false;
+    }
+}
+
+
+QVariant TemplateRect::itemChange(GraphicsItemChange change, const QVariant &value) {
+    if (change == QGraphicsItem::ItemSelectedChange) {
+        resize_button->setVisible(value.toBool());
+    }
+
+    return QGraphicsItem::itemChange(change, value);
 }
